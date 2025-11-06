465804d00cf47f025b2c4c842db21afeea7593c6
Zenny <zennynyangwa@gmail.com>
2025-11-06 14:09:54 +0200

    login branch creation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+loginForm=new FormGroup({
+  email:new FormControl('', [Validators.required, Validators.email]),
+  password:new FormControl('',Validators.required),
+});
 
+constructor() { }
+login(){
+  // Login logic here
+}
 }
